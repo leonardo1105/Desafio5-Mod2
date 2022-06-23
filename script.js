6d8a4804bce104d38a4ee8995b74c6f7ffbc8dd9
Leonardo Nunes <leonardoportelanunes@gmail.com>
2022-06-23 11:21:28 -0400

    Desafio Finalizado

---
+++ script.js
@@ -0,0 +1,18 @@
+let listaDeCompras=['Feijão','Arroz','Farinha','Calabresa','Batata'];
+console.log(listaDeCompras)
+listaDeCompras.unshift('Cenoura')
+console.log(listaDeCompras)
+
+listaDeCompras.pop()
+console.log(listaDeCompras)
+
+listaDeCompras.push('Beterraba','Tomate')
+console.log(listaDeCompras)
+
+listaDeCompras.shift()
+console.log(listaDeCompras)
+
+let numbers=[7,5,6,3,8,9,2,1,4]
+numbers.sort()
+
+console.log(numbers)
